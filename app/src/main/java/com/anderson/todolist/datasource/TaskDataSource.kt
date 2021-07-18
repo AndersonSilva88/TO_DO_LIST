25679e6cb86eacc4c03b275bcc41019c4bace484
perkles <anderson.os88@hotmail.com>
2021-07-18 17:38:32 -0300

    app de lista de tarefa em construção

---
+++ app/src/main/java/com/anderson/todolist/datasource/TaskDataSource.kt
@@ -0,0 +1,13 @@
+package com.anderson.todolist.datasource
+
+import com.anderson.todolist.model.Task
+
+object TaskDataSource {
+    private val list = arrayListOf<Task>()
+
+    fun getList() = list
+
+    fun insertTask(task: Task) {
+        list.add(task.copy(id = list.size + 1))
+    }
+}
---
+++ app/src/main/java/com/anderson/todolist/model/Task.kt
@@ -0,0 +1,9 @@
+package com.anderson.todolist.model
+
+data class Task(
+
+    val title : String,
+    val hour : String,
+    val date : String,
+    val id: Int = 0
+)
